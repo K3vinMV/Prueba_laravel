65c7bc457d651c185ec351e129494fee8703a47d
KevCUCEI <kevinmelendez3535@gmail.com>
2024-09-11 16:54:08 -0600

    CRUD agregado

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ContactoController;
+use App\Http\Controllers\NoticiaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,3 +24,5 @@ Route::get('/contacto/{tipo_persona?}', [ContactoController::class, "formulario"
 Route::post('/contacto-recibe', [ContactoController::class, "newContact"]);
 
 Route::get('lista', [ContactoController::class, "lista"]);
+
+Route::resource('noticias', NoticiaController::class);
